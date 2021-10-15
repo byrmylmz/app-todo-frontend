7c5b17aa74ae6252b44940f99fc3b86febc0aa08
byrmylmaz <bayramyilmaz061@gmail.com>
2021-10-15 14:22:19 +0300

    Layouts pages added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import Master from './components/layouts/Master'
 import store from './store'
 import './index.css'
 window.eventBus=new Vue()
@@ -9,5 +10,5 @@ Vue.config.productionTip = false
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(Master)
 }).$mount('#app')
